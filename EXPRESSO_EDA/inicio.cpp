91402fd2e795b75b8417b6c0505f9010fef19550
JoaoCupido <61253001+JoaoCupido@users.noreply.github.com>
2020-05-11 14:04:05 +0100

    3 novas funçoes: lerFicheiroPrimeiroNome , lerFicheiroUltimoNome e lerFicheiroParagens (Cupido 11/5/20)

--- EXPRESSO_EDA/inicio.cpp
+++ EXPRESSO_EDA/inicio.cpp
@@ -1,9 +1,10 @@
 #include "inicio.h"
 #include <fstream>
-#include<iostream>
-#include<iomanip>
-#include<locale>
-#include<string>
+#include <iostream>
+#include <iomanip>
+#include <locale>
+#include <string>
+
 using namespace std;
 
 void fila_espera(string u_nome) //cupido e pedro 11/5
@@ -55,4 +56,52 @@ void cria_autocarros()
 void cria_paragens() //pedro e cupido
 {
     int Paragens = rand() % 6 + 4; //de 4 a 9
+}
+
+void lerFicheiroPrimeiroNome(string* listaprimeironome) { //Cupido 11/05/20
+    ifstream myFile("primeiro_nome.txt");
+    string line = "";
+    int i = 0;
+    if (myFile.is_open()) {  //verificar se o ficheiro existe
+        while (getline(myFile, line)) {
+            listaprimeironome[i] = line;
+            i++;
+        }
+    }
+    myFile.close();
+    /*for (int j = 0; j < NUM_P_NOMES; j++) {
+        cout << listaprimeironome[j] << endl;
+    }*/
+}
+
+void lerFicheiroUltimoNome(string* listaultimonome) { //Cupido 11/05/20
+    ifstream myFile("ultimo_nome.txt");
+    string line = "";
+    int i = 0;
+    if (myFile.is_open()) {  //verificar se o ficheiro existe
+        while (getline(myFile, line)) {
+            listaultimonome[i] = line;
+            i++;
+        }
+    }
+    myFile.close();
+    /*for (int j = 0; j < NUM_U_NOMES; j++) {
+        cout << listaultimonome[j] << endl;
+    }*/
+}
+
+void lerFicheiroParagens(string* listaparagens) { //Cupido 11/05/20
+    ifstream myFile("paragens.txt");
+    string line = "";
+    int i = 0;
+    if (myFile.is_open()) {  //verificar se o ficheiro existe
+        while (getline(myFile, line)) {
+            listaparagens[i] = line;
+            i++;
+        }
+    }
+    myFile.close();
+    /*for (int j = 0; j < NUM_PARAGENS; j++) {
+        cout << listaparagens[j] << endl;
+    }*/
 }

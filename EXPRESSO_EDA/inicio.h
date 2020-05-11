0a8e2cbbc8c4163df825baf7fe4bb415cfd6f80d
JoaoCupido <61253001+JoaoCupido@users.noreply.github.com>
2020-05-11 16:37:25 +0100

    fixed

--- EXPRESSO_EDA/inicio.h
+++ EXPRESSO_EDA/inicio.h
@@ -1,10 +1,8 @@
 #pragma once
-#include <iostream>
-#include <string>
-#include "Fila.h"
 #define NUM_P_NOMES 44
 #define NUM_U_NOMES 97
 #define NUM_PARAGENS 54 //Cupido 11/05/20
+#include <iostream>
 using namespace std;
 
 struct Pessoa { //mai 11/05
@@ -18,9 +16,9 @@ struct Item { //mai 11/05
 	Pessoa pessoa;
 };
 
-// void fila_espera(string u_nome);
+void fila_espera(string u_nome);
 
-Pessoa criaPessoa(string pnomes[], string unomes[]); //mai 11/05
+Pessoa Criapessoa(string pnomes[NUM_P_NOMES], string unomes[NUM_U_NOMES]); //mai 11/05
 
 bool saida_passageiros();
 
@@ -29,6 +27,6 @@ void cria_autocarros();
 void cria_paragens();
 
 //Cupido 11/05/20
-void lerFicheiroPrimeiroNome(string listaprimeironome[]);
-void lerFicheiroUltimoNome(string listaultimonome[]);
-void lerFicheiroParagens(string listaparagens[]);
+void lerFicheiroPrimeiroNome(string* listaprimeironome);
+void lerFicheiroUltimoNome(string* listaultimonome);
+void lerFicheiroParagens(string* listaparagens);

7f50ec2987c06e56072afa0a1d3efb5fcb7a03a1
JoaoCupido <61253001+JoaoCupido@users.noreply.github.com>
2020-05-11 16:36:31 +0100

    fixed

--- EXPRESSO_EDA/Fila.h
+++ EXPRESSO_EDA/Fila.h
@@ -1,9 +1,8 @@
 #pragma once
 #include <iostream>
-#include <string>
 #include "inicio.h"
 
 using namespace std;
 
+void inicializaFila(Item* primeiro, string* listaPnomes, string* listaUnomes);
 void entraFila(Item* primeiro, Pessoa novapessoa);
-void inicializaFila(Item* primeiro, string listaPnomes[], string listaUnomes[]);

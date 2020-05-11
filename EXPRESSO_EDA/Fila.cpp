027342aff5b3e8c6d402a31a3506eb9b606da486
JoaoCupido <61253001+JoaoCupido@users.noreply.github.com>
2020-05-11 16:36:15 +0100

    fixed

--- EXPRESSO_EDA/Fila.cpp
+++ EXPRESSO_EDA/Fila.cpp
@@ -1,29 +1,28 @@
-#pragma once
 #include "Fila.h"
 #include "inicio.h"
 #include <iostream>
 
 using namespace std;
 
+void inicializaFila(Item* primeiro, string* listaPnomes, string* listaUnomes) {
+	primeiro->pessoa = Criapessoa(listaPnomes, listaUnomes);
+	for (int i = 0; i < 30; i++) {
+		Pessoa novapessoa = Criapessoa(listaPnomes, listaUnomes);
+		entraFila(primeiro, novapessoa);
+	}
+}
 void entraFila(Item* primeiro, Pessoa novapessoa) {
 	Item* aux = new Item;
 	aux->pessoa = novapessoa;
 	aux->seguinte = NULL;
-	if (primeiro == NULL)
+	if (primeiro == NULL) {
 		primeiro = aux;
+	}
 	else {
 		Item* temp = primeiro;
 		while (temp->seguinte != NULL) {
 			temp = temp->seguinte;
 		}
 		temp->seguinte = aux;
-	}
-}
-
-void inicializaFila(Item* primeiro, string listaPnomes[], string listaUnomes[]) {
-	primeiro->pessoa = criaPessoa(listaPnomes, listaUnomes);
-	for (int i = 0; i < 30; i++) {
-		Pessoa novapessoa = criaPessoa(listaPnomes, listaUnomes);
-		entraFila(primeiro, novapessoa);
 	}
 }

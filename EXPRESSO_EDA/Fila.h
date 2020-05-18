1cb4506f9ecccf6e25f042710b41704019c0dbf0
JoaoCupido <61253001+JoaoCupido@users.noreply.github.com>
2020-05-18 15:40:15 +0100

    18/05

--- EXPRESSO_EDA/Fila.h
+++ EXPRESSO_EDA/Fila.h
@@ -6,3 +6,5 @@ using namespace std;
 
 void inicializaFila(Item* primeiro, string* listaPnomes, string* listaUnomes);
 void entraFila(Item* primeiro, Pessoa novapessoa);
+
+void imprimeFila(Item* primeiro, string* listaPnomes, string* listaUnomes);

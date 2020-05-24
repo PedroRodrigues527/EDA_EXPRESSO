4cc4ef1dc494b944330c018f7762d437875ae4ac
Pedro Rodrigues <pedro.miguel.rodrigues00@gmail.com>
2020-05-24 14:41:17 +0100

    Update percurso.h

--- EXPRESSO_EDA/percurso.h
+++ EXPRESSO_EDA/percurso.h
@@ -3,12 +3,13 @@
 #include "Fila.h"
 using namespace std;
 
+void alteraMotorista(percurso& pe);
 
 struct autocarro { // mai
 
 	Item* primeiro; // apontador para lista ligada de passageiros
 
-	int matricula = 0; // inicializado a 0, mas È um numero hexadecimal de 1000 a FFFF (4096 a 65535);
+	int matricula = 0; // inicializado a 0, mas √© um numero hexadecimal de 1000 a FFFF (4096 a 65535);
 	int capacidade; // tamanho da lista ligada
 
 	string p_nome_C; // do condutor
@@ -16,7 +17,7 @@ struct autocarro { // mai
 };
 
 
-struct arvore_bilhetes { // arvore de procura bin·ria // mai
+struct arvore_bilhetes { // arvore de procura bin√°ria // mai
 	struct bilhete {
 		int num;
 		bilhete* esq;
